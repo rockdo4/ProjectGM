1a3a99e2ae75a20b3100338c9d0698dcbedd93d6
김주현 <gurados@gmail.com>
2023-11-20 15:21:43 +0900

    NuGet 패키지 관리  - Newtonsoft JSON과 CsvHelper를 설치하기 위한 패키지.
    CsvTable
     - 외부 라이브러리 CsvHelper를 이용한 추상 데이터 테이블 객체.
    
    CsvTableMgr
     - 외부 라이브러리 CsvHelper를 이용한 데이터 테이블을 관리하기 위한 객체.
    
    SaveData
     - Json 파일에 정보를 저장하기 위한 추상 객체
    
    SaveLoadSystem
     - Json 파일에 저장과 불러오기 기능을 위한 객체.

---
+++ Assets/Scripts/SaveLoadSystem/SaveData.cs
@@ -0,0 +1,154 @@
+using System.Collections.Generic;
+
+public abstract class SaveData
+{
+    public int Version { get; set; }
+
+    public abstract SaveData VersionUp();
+}
+
+public class SaveDataV1 : SaveData
+{
+    public SaveDataV1() 
+    {
+        Version = 1;
+    }
+
+    public int Gold { get; set; } = 0;
+
+    public override SaveData VersionUp()
+    {
+        var data = new SaveDataV2();
+        data.Gold = Gold;
+        
+        return data;
+    }
+}
+
+public class SaveDataV2 : SaveData 
+{
+    public SaveDataV2()
+    {
+        Version = 2;
+    }
+
+    public int Gold { get; set; } = 0;
+    public int HighScore { get; set; } = 0;
+
+    public override SaveData VersionUp()
+    {
+        return null;
+    }
+}
+/*
+public class SaveDataV3 : SaveData
+{
+    public SaveDataV3()
+    {
+        Version = 3;
+    }
+
+    public int HighScore { get; set; } = 0;
+    public int Gold { get; set; } = 0;
+    public int Upgrade_HealthUP { get; set; } = 0;
+    public int Upgrade_GoldUP { get; set; } = 0;
+    public int Upgrade_SpeedDown { get; set; } = 0;
+
+    public override SaveData VersionUp()
+    {
+        var data = new SaveDataV4();
+        data.HighScore = HighScore;
+        data.Gold = Gold;
+        data.Upgrade_HealthUP = Upgrade_HealthUP;
+        data.Upgrade_GoldUP = Upgrade_GoldUP;
+        data.Upgrade_SpeedDown = Upgrade_SpeedDown;
+
+        return data;
+    }
+}
+
+public class SaveDataV4 : SaveData
+{
+    public SaveDataV4()
+    {
+        Version = 4;
+    }
+
+    public int HighScore { get; set; } = 0;
+    public int Gold { get; set; } = 0;
+    public int Upgrade_HealthUP { get; set; } = 0;
+    public int Upgrade_GoldUP { get; set; } = 0;
+    public int Upgrade_SpeedDown { get; set; } = 0;
+    public int Stage { get; set; } = 1;
+
+    public override SaveData VersionUp()
+    {
+        var data = new SaveDataV5();
+        data.HighScore = HighScore;
+        data.Gold = Gold;
+        data.Upgrade_HealthUP = Upgrade_HealthUP;
+        data.Upgrade_GoldUP = Upgrade_GoldUP;
+        data.Upgrade_SpeedDown = Upgrade_SpeedDown;
+        data.Stage = Stage;
+
+        return data;
+    }
+}
+
+public class SaveDataV5 : SaveData
+{
+    public SaveDataV5()
+    {
+        Version = 5;
+    }
+
+    public int HighScore { get; set; } = 0;
+    public int Gold { get; set; } = 0;
+    public int Upgrade_HealthUP { get; set; } = 0;
+    public int Upgrade_GoldUP { get; set; } = 0;
+    public int Upgrade_SpeedDown { get; set; } = 0;
+    public int Stage { get; set; } = 1;
+    public WeaponID EquipWeapon { get; set; } = WeaponID.Starter;
+    public List<WeaponID> UnlockList { get; set; } = new List<WeaponID>();
+
+    public override SaveData VersionUp()
+    {
+        var data = new SaveDataV6();
+        data.HighScore = HighScore;
+        data.Gold = Gold;
+        data.Upgrade_HealthUP = Upgrade_HealthUP;
+        data.Upgrade_GoldUP = Upgrade_GoldUP;
+        data.Upgrade_SpeedDown = Upgrade_SpeedDown;
+        data.Stage = Stage;
+        data.EquipWeapon = EquipWeapon;
+        data.UnlockList = UnlockList;
+
+        return data;
+    }
+}
+
+public class SaveDataV6 : SaveData // Current
+{
+    public SaveDataV6()
+    {
+        Version = 6;
+    }
+
+    public int HighScore { get; set; } = 0;
+    public int Gold { get; set; } = 0;
+    public int Upgrade_HealthUP { get; set; } = 0;
+    public int Upgrade_GoldUP { get; set; } = 0;
+    public int Upgrade_SpeedDown { get; set; } = 0;
+    public int Stage { get; set; } = 1;
+    public WeaponID EquipWeapon { get; set; } = WeaponID.Starter;
+    public List<WeaponID> UnlockList { get; set; } = new List<WeaponID>();
+    public float masterVol { get; set; } = 1.0f;
+    public float musicVol { get; set; } = 1.0f;
+    public float sfxVol { get; set; } = 1.0f;
+
+    public override SaveData VersionUp()
+    {
+        return null;
+    }
+}
+*/
---
+++ Assets/Scripts/SaveLoadSystem/SaveLoadSystem.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public static class SaveLoadSystem
+{
+    public static int RecentlyVersion { get; } = 1; // Version Change?
+
+    public static string SaveDirectory
+    {
+        get
+        {
+            return $"{Application.persistentDataPath}/Save";
+        }
+    }
+
+    public static void Save(SaveData data, string fileName)
+    {
+        if (!Directory.Exists(SaveDirectory))
+        {
+            Directory.CreateDirectory(SaveDirectory);
+        }
+
+        var path = Path.Combine(SaveDirectory, fileName);
+        using (var writer = new JsonTextWriter(new StreamWriter(path))) // Add Converter
+        {
+            var serializer = new JsonSerializer();
+            serializer.Converters.Add(new Vector3Converter());
+            serializer.Converters.Add(new QuaternionConverter());
+
+            serializer.Serialize(writer, data);
+        }
+    }
+
+    public static SaveData Load(string fileName)
+    {
+        var path = Path.Combine(SaveDirectory, fileName);
+        if (!File.Exists(path))
+        {
+            return null;
+        }
+
+        SaveData result = null;
+        int version = 0;
+
+        var json = File.ReadAllText(path);
+        using (var reader = new JsonTextReader(new StringReader(json)))
+        {
+            var jObj = JObject.Load(reader);
+            version = jObj["Version"].Value<int>();
+        }
+        using (var reader = new JsonTextReader(new StringReader(json)))
+        {
+            var serializer = new JsonSerializer();
+            serializer.Converters.Add(new Vector3Converter());
+            serializer.Converters.Add(new QuaternionConverter());
+
+            switch (version) // Add Version?
+            {
+                case 1:
+                    result = serializer.Deserialize<SaveDataV1>(reader);
+                    break;
+                case 2:
+                    result = serializer.Deserialize<SaveDataV2>(reader);
+                    break;
+            }
+            while (result.Version < RecentlyVersion)
+            {
+                result = result.VersionUp();
+            }
+        }
+        return result;
+    }
+}
