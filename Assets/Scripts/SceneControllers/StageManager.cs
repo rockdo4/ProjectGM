7c11445c4f6f5604e92f887723913849badd9804
김주현 <gurados@gmail.com>
2023-12-12 19:08:57 +0900

    StageScene  - 스테이지를 선택하고 해당 스테이지에 입장하기 위한 씬.  - 디자인 60% 완성.
    StageManager
     - StageScene에서 스테이지를 관리하는 객체.
     - 기능 구현 필요.

---
+++ Assets/Scripts/SceneControllers/StageManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [Header("Global Panel")]
+    public GameObject globalPanel;
+}
