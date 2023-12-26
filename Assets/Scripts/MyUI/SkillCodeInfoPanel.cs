0b36392a77534b95ba9482283c712ac3924c09cd
김주현 <gurados@gmail.com>
2023-12-26 18:12:32 +0900

    Title  - 스킬코드 확장 패널 이동.
    LockerButton
     - PC버전 전처리문 추가.
    
    SkillCodeInfoPanel
     - PC버전 전처리문 추가.

--- Assets/Scripts/MyUI/SkillCodeInfoPanel.cs
+++ Assets/Scripts/MyUI/SkillCodeInfoPanel.cs
@@ -8,4 +8,11 @@ public class SkillCodeInfoPanel : MonoBehaviour
 
     [Header("레벨 텍스트")]
     public TextMeshProUGUI levelText;
+
+    private void OnEnable()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        transform.localScale = Vector3.one;
+#endif
+    }
 }
