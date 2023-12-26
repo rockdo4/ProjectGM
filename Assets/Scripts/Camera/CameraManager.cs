691cc481717c4543f5c89cc02ab38722e964866a
이영재 <dudwo1830@naver.com>
2023-12-26 17:59:39 +0900

    카메라 클로즈업 준비

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -1,6 +1,4 @@
 using Cinemachine;
-using System.Collections;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
@@ -18,8 +16,11 @@ public class CameraManager : MonoBehaviour
     }
     private static CameraManager m_instance;
 
-    private CinemachineVirtualCamera virtualCamera;
     private CinemachineBasicMultiChannelPerlin noise;
+
+    private CinemachineVirtualCamera[] playerCameras;
+    private CinemachineVirtualCamera[] enemyCameras;
+    private CinemachineVirtualCamera currentVirtualCamera;
 
     private void Awake()
     {
@@ -27,8 +28,49 @@ public class CameraManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        virtualCamera = GetComponent<CinemachineVirtualCamera>();
-        noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        playerCameras = GameObject.FindWithTag(Tags.player)?.GetComponentsInChildren<CinemachineVirtualCamera>();
+        enemyCameras = GameObject.FindWithTag(Tags.enemy)?.GetComponentsInChildren<CinemachineVirtualCamera>();
+
+        currentVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        noise = currentVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void Start()
+    {
+        playerCameras = GameObject.FindWithTag(Tags.player)?.GetComponentsInChildren<CinemachineVirtualCamera>();
+        enemyCameras = GameObject.FindWithTag(Tags.enemy)?.GetComponentsInChildren<CinemachineVirtualCamera>();
+        SetCamera(Tags.player);
+    }
+
+    private void Update()
+    {
+        if (true)
+        {
+
+        }
+    }
+
+    public void SetCamera(string tag, int index = 0)
+    {
+        if (tag == Tags.player)
+        {
+            if (playerCameras.Length <= index)
+            {
+                return;
+            }
+            currentVirtualCamera = playerCameras[index];
+        }
+        else if (tag == Tags.enemy)
+        {
+            if (enemyCameras.Length <= index)
+            {
+                return;
+            }
+            currentVirtualCamera = enemyCameras[index];
+        }
+
+        currentVirtualCamera.enabled = true;    
     }
 
     public void Noise(float amplitudeGain, float frequencyGain, Vector3 pivotOffset = default)
