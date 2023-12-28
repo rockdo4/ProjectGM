533fde57c9d9ae78899161f3094ddc5cb909aa0b
이영재 <dudwo1830@naver.com>
2023-12-28 17:55:02 +0900

    몬스터 string table 최신화 스테이지 테이블을 통해 게임 세팅 위로 인한 빌드세팅 수정 몬스터 약점 데이터 추가

--- Assets/Scripts/Common/InGameManager.cs
+++ Assets/Scripts/Common/InGameManager.cs
@@ -17,71 +17,71 @@ public class InGameManager : MonoBehaviour
     }
     private static InGameManager m_instance;
 
+    [Header("EnemySO")]
+    public EnemySO enemySO;
+
     [System.Serializable]
     private class PlayerData
     {
-        public Player prefab;
+        public Player player;
         public GameObject infoUI;
         public Transform startTransform;
     }
     [System.Serializable]
     private class EnemyData
     {
-        public EnemyAI prefab;
+        public EnemyAI enemy { get; set; }
         public GameObject infoUI;
         public Transform startTransform;
     }
 
     [SerializeField] private PlayerData playerData;
     [SerializeField] private EnemyData enemyData;
-    [SerializeField] private Player player;
     private Slider playerHp;
-    [SerializeField] private EnemyAI enemy;
     private Slider enemyHp;
     private Slider evadePoint;
+    private EnemyTable.Data enemyInfo;
 
     [SerializeField] public Slider timeSlider;
-    private EnemyTable.Data enemyInfo;
 
     private void Awake()
     {
-        var stageID = PlayerPrefs.GetInt("StageID");
-        if (stageID != 0)
-        {
-            var stageInfo = CsvTableMgr.GetTable<StageTable>().dataTable[stageID];
-            enemyInfo = CsvTableMgr.GetTable<EnemyTable>().dataTable[stageInfo.monster_id];
-        }
-
         if (Instance != this)
         {
             Destroy(gameObject);
         }
 
+        var stageID = PlayerPrefs.GetInt("StageID");
+        var stageTable = CsvTableMgr.GetTable<StageTable>().dataTable;
+#if UNITY_EDITOR
+        if (!stageTable.ContainsKey(stageID))
+        {
+            stageID = 1101001; //default;
+        }
+#endif
+        if (!stageTable.ContainsKey(stageID))
+        {
+            Debug.LogWarning("Not Found StageInfo!");
+        }
+
+        var stageInfo = stageTable[stageID];
+
         timeSlider.minValue = 0f;
-        timeSlider.maxValue = 180f; //Temp
+        timeSlider.maxValue = stageInfo.time_limit;
         timeSlider.value = 0f;
         timeSlider.onValueChanged.AddListener(OnTimerChanged);
 
         playerData.infoUI.SetActive(false);
         enemyData.infoUI.SetActive(false);
-        var prefabCheck = playerData.prefab == null || enemyData.prefab == null;
-        var transformCheck = playerData.startTransform == null || enemyData.startTransform == null;
-        if (prefabCheck)
-        {
-            return;
-        }
-        if (transformCheck)
-        {
-            return;
-        }
-        player = Instantiate(playerData.prefab, playerData.startTransform.position, Quaternion.identity);
-        enemy = Instantiate(enemyData.prefab, enemyData.startTransform.position, Quaternion.identity);
-        
+
+        enemyData.enemy = enemySO.MakeEnemy(stageInfo.monster_id, enemyData.startTransform).GetComponent<EnemyAI>();
+        enemyData.enemy.gameObject.SetActive(true);
+        enemyInfo = CsvTableMgr.GetTable<EnemyTable>().dataTable[stageInfo.monster_id];
     }
 
     private void Start()
     {
-        if (player == null || enemy == null)
+        if (playerData.player == null || enemyData.enemy == null)
         {
             return;
         }
@@ -90,7 +90,7 @@ public class InGameManager : MonoBehaviour
 
     private void Update()
     {
-        if (player == null || enemy == null)
+        if (playerData.player == null || enemyData.enemy == null)
         {
             return;
         }
@@ -105,22 +105,22 @@ public class InGameManager : MonoBehaviour
     private void Init()
     {
         {
-            playerData.infoUI.GetComponentInChildren<TextMeshProUGUI>().text = player.name;
+            playerData.infoUI.GetComponentInChildren<TextMeshProUGUI>().text = playerData.player.name;
             playerHp = playerData.infoUI.transform.Find("Hp").GetComponent<Slider>();
             playerHp.minValue = 0f;
-            playerHp.maxValue = player.Stat.HP;
+            playerHp.maxValue = playerData.player.Stat.HP;
         }
         {
             var st = CsvTableMgr.GetTable<StringTable>().dataTable;
             enemyData.infoUI.GetComponentInChildren<TextMeshProUGUI>().text = st[enemyInfo.name];
             enemyHp = enemyData.infoUI.transform.Find("Hp").GetComponent<Slider>();
             enemyHp.minValue = 0f;
-            enemyHp.maxValue = enemy.Stat.HP;
+            enemyHp.maxValue = enemyData.enemy.Stat.HP;
         }
         {
             evadePoint = enemyData.infoUI.transform.Find("EvadePoint").GetComponent<Slider>();
             evadePoint.minValue = 0f;
-            evadePoint.maxValue = player.Stat.maxEvadePoint;
+            evadePoint.maxValue = playerData.player.Stat.maxEvadePoint;
         }
         playerData.infoUI.SetActive(true);
         enemyData.infoUI.SetActive(true);
@@ -131,16 +131,16 @@ public class InGameManager : MonoBehaviour
     public void UpdateUI()
     {
         timeSlider.value += Time.deltaTime;
-        playerHp.value = player.HP;
-        enemyHp.value = enemy.HP;
-        evadePoint.value = player.evadePoint;
+        playerHp.value = playerData.player.HP;
+        enemyHp.value = enemyData.enemy.HP;
+        evadePoint.value = playerData.player.evadePoint;
     }
 
     private void OnTimerChanged(float value)
     {
         if (timeSlider.value >= timeSlider.maxValue)
         {
-            GameManager.instance.GameOver(player);
+            GameManager.instance.GameOver(playerData.player);
         }
     }
 }
