43628b51958bc57ced6312ef2147fe429963f89c
이영재 <dudwo1830@naver.com>
2023-12-28 15:17:54 +0900

    Enemy - ID 연결, 약점 스탯 추가

--- Assets/EnemyStat.cs
+++ Assets/EnemyStat.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyStat", menuName = "EnemyStat")]
 public class EnemyStat : Stat
 {
+    [Header("약점 속성")]
+    public AttackType weaknessType = AttackType.None;
+
     public override Attack CreateAttack(LivingObject attacker, LivingObject defender, bool groogy = false)
     {
         float damage = attacker.stat.AttackDamage;
