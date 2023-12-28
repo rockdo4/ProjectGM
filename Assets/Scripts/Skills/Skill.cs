b704d9bfffb6be2eea08eaae0fd1918cd108ac90
김주현 <gurados@gmail.com>
2023-12-28 15:33:21 +0900

    <데이터 테이블 갱신>
    armor_table.csv, skill_table.csv, skillcode_table.csv, string_table2.csv, string_table3.csv, string_table6.csv 갱신.
    
    Skill
     - 시작 시 플레이어 스탯을 가져올 Init 함수 추가.

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
+    private PlayerStat stat;
+
+    public void Init()
+    {
+        stat = GetComponent<Player>().Stat;
+    }
+
     public int level { get; set; } = 1;
 }
+
+/*
+public class TestSkill : Skill
+{
+    private void Start()
+    {
+        Init();
+    }
+}
+*/
